cfa389d73dc7f7abc035db4feda884e8dceedb28
“Harsh <harshwalia449@yahoo.com>
2025-03-27 04:39:59 +0530

    removed build folder

---
+++ apps/excalidraw-frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"YtZpPqUrY5gtejzQTk/hFFEGmqPHZAoD6O0rluoAuow=\"}"
---
+++ apps/excalidraw-frontend/.next/static/cKHkeuK8NTgLHQhDJ-P9m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a8b829ff75b37b7f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
